db92034830160c0e82eb3516d33243c5f620a8c4
Kerem Bozdas <krmbzds.github@gmail.com>
2014-03-14 23:14:07 +0200

    Change formatting

--- cursemix.rb
+++ cursemix.rb
@@ -1,9 +1,18 @@
 require 'sinatra'
 require 'haml'
 
-prefix = ["nut", "dildo", "testicle", "trash", "wench", "meat", "mouth", "whore", "fetus", "prick", "rectum", "scrotum", "pecker", "sissy", "'gina", "muff", "panty", "cock", "fuck", "twat", "douche", "nipple", "bitch", "queef", "pussy", "mother", "cunt", "tampon", "ball", "ass", "sperm", "cooch", "juice", "clit", "jizz", "fart", "granny", "tit", "cum", "schlong", "diaper", "wiener", "fetus", "scum", "slut", "crap", "butt", "snot", "piss", "nut", "dildo", "testicle"]
+prefix = ["nut", "dildo", "testicle", "trash", "wench", "meat", "mouth", "whore", "fetus", "prick", "rectum", 
+          "scrotum", "pecker", "sissy", "'gina", "muff", "panty", "cock", "fuck", "twat", "douche", "nipple", 
+          "bitch", "queef", "pussy", "mother", "cunt", "tampon", "ball", "ass", "sperm", "cooch", "juice", 
+          "clit", "jizz", "fart", "granny", "tit", "cum", "schlong", "diaper", "wiener", "fetus", "scum", 
+          "slut", "crap", "butt", "snot", "piss", "nut", "dildo", "testicle"]
 
-suffix = ["bag", "wad", "biscuit", "waffle", "wipe", "chomper", "nibbler", "flaps", "sandwich", "skank", "jockey", "lips", "munch", "slammer", "blower", "stain", "wrangler", "fungus", "pooper", "pincher", "beater", "licker", "tickler", "clot", "lover", "dangler", "farm", "boner", "packer", "slime", "gobbler", "folds", "gargler", "biter", "donkey", "twister", "booger", "nugget", "eater", "pooper", "dumpster", "monster", "wanker", "plug", "sucker", "bucket", "fucker", "hole", "jammer", "glob", "shitter", "junkie", "jacket", "blower", "fondler"]
+suffix = ["bag", "wad", "biscuit", "waffle", "wipe", "chomper", "nibbler", "flaps", "sandwich", "skank", 
+          "jockey", "lips", "munch", "slammer", "blower", "stain", "wrangler", "fungus", "pooper", "pincher",
+          "beater", "licker", "tickler", "clot", "lover", "dangler", "farm", "boner", "packer", "slime", 
+          "gobbler", "folds", "gargler", "biter", "donkey", "twister", "booger", "nugget", "eater", "pooper", 
+          "dumpster", "monster", "wanker", "plug", "sucker", "bucket", "fucker", "hole", "jammer", "glob", 
+          "shitter", "junkie", "jacket", "blower", "fondler"]
 
 spc_curses = ["shit faced cock master", "donkey raping shit eater", "pig fucker", "uncle fucker",
               "fucking fat ass", "fuckity-fuck-fuck-fuck", "butt-fuckin' son of a bitch", "buttfucker",
@@ -21,7 +30,7 @@ def random_spc_curse(spc_curses)
 end
 
 get '/' do
-  haml :index, :locals => {:curse => random_curse(prefix, suffix)}
+  haml :index, :locals => { :curse => random_curse(prefix, suffix) }
 end
 
 get '/spc' do
